6e62bea04fa2a821cd56db363081c2ca2446e6f7
WahajAhmedIqbal <wahaj465@gmail.com>
2022-06-30 05:13:31 +0500

    header

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,21 +1,9 @@
 import React from 'react'
-import { Container, Navbar, Nav } from 'react-bootstrap'
 
 const HeaderComponent = () => {
     return (
         <div>
-            <Navbar bg="light" expand="lg">
-                <Container>
-                    <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
-                    <Navbar.Toggle aria-controls="basic-navbar-nav" />
-                    <Navbar.Collapse id="basic-navbar-nav">
-                        <Nav className="mr-auto">
-                            <Nav.Link href="#home">Home</Nav.Link>
-                            <Nav.Link href="#link">Link</Nav.Link>
-                        </Nav>
-                    </Navbar.Collapse>
-                </Container>
-            </Navbar>
+
         </div>
     )
 }
